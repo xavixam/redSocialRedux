97fb3e5e59edce356b13b28abd55fb5b1edf7751
xavi <antunez.xavi.grao@gmail.com>
2025-01-09 11:58:39 +0100

    Profile con info del usuario

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+    const { user } = useSelector((state) => state.auth);
+
   return (
-    <div>Profile</div>
+    <div>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+    
+    </div>
   )
 }
 
